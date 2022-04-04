56c8025569dddbec8da974caa1b8c7438eb0c503
greg <gregargue@protonmail.com>
2022-04-04 13:41:16 -0400

    Understanding Django

---
+++ projects/2020/x/wiki/encyclopedia/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import views
+
+
+urlpatterns = [
+
+    #if path is empty run index function in encyclopedia/urls.py
+    path("", views.index, name="index")
+    
+]
